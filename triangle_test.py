343b8f8ae269bd56ef06de560e6b528c3363d93c
Alexander Varney <alex@varney.me>
2021-09-23 23:19:16 -0400

    test is failing

--- triangle_test.py
+++ triangle_test.py
@@ -6,9 +6,7 @@ def square(x):
 
 def exists_right_triangle(x: float, y: float, z: float) -> bool:
   
-  squares = sorted(map(square, (x, y, z)))
-
-  return abs(((squares[0] + squares[1]) - squares[2])) < 0.01
+  return False
 
 def exists_triangle(x, y, z):
   return sorted((x, y, z))[2] <= sorted((x,y,z))[1] + sorted((x, y, z))[0]
